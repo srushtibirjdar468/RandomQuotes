adbc75556fcd791103d3bce585ddcc944c0140fa
birajdaranil675 <birajdaranil675@gmail.com>
2024-02-29 02:29:17 +0530

    added more styling

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 fetchQuote=()=>{
+    const container = document.getElementById('container');
     fetch('https://type.fit/api/quotes').then((data)=>{
         console.log('data fetched from url')
         return data.json()
     })
     .then((data)=>{
-        let random =Math.floor(Math.random()*data.length);
-        document.getElementById('quote').innerText=data[random].text;
-        document.getElementById('author').innerText=data[random].author;
+        let counter = 1;
+        data.forEach(d => {
+            const mydiv = document.createElement('div');
+            mydiv.className = 'quote'
+            mydiv.id = 'div' + counter;
+
+            const heading = document.createElement('h2');
+            heading.innerHTML = d.text
+            mydiv.appendChild(heading);
+
+            const description = document.createElement('p');
+            description.innerHTML = d.author;
+            mydiv.appendChild(description);
+
+            container.appendChild(mydiv);
+            counter++;
+        });
+        
     })
+
+    
 }
 fetchQuote()
